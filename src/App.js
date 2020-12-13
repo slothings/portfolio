5a750e49f9110e94dd7adc8523c5a193056abd22
Slothing <seth.r.martineau@gmail.com>
2020-12-13 11:57:40 -0700

    got porfolio and contact pages initially setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,10 +13,9 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={ } />
-        <Route exact path="/" component={ } />
-        <Route exact path="/" component={ } />
-        <Route exact path="/" component={ } />
+        <Route exact path="/" component={About} />
+        <Route exact path="/" component={Portfolio} />
+        <Route exact path="/" component={Contact} />
         <Footer />
       </div>
     </Router>
