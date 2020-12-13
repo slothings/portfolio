309e1a95a9f9506d2b104e0adc8fd314e0b0751e
Slothing <seth.r.martineau@gmail.com>
2020-12-13 12:25:06 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Header from "./components/Header";
+import Footer from "./components/Footer/Footer";
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <Route exact path="/" component={About} />
-        <Route exact path="/" component={Portfolio} />
-        <Route exact path="/" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
         <Footer />
       </div>
     </Router>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="footer auto fixed-bottom text-center">
+          <div class="card text-center">
+            <h6 class="card-footer text-muted"> Copyright &copy; 2020 Seth Martineau</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
