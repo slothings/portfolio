a66c819cd97d0db7445437d32f92e2a9618b4468
Slothing <seth.r.martineau@gmail.com>
2020-12-15 00:03:27 -0700

    fixed css, added resume

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,30 +5,22 @@ function About() {
   return (
     <divabout>
 
-      <div1 class="card-deck auto m-0 p-3 paper">
+      <div class="card-body text-center text-white float-right overcast">
 
-        <div>
-          <img className="selfie" src={selfie} alt="Selfie"></img>
-        </div>
+        <img className="selfie img-fluid p-3" src={selfie} alt="Selfie"></img>
 
-        <div class="card">
-          <div class="card-body text-center text-white overcast">
-
-            <p>Hello! My name is Seth Martineau and I'm a 26 year old graduate from the University of
-            Utah. I am currently enrolled in a coding bootcamp because I wanted a career change. Coding so far has been
-            challenging and rewarding. I hope to learn more and am able to use these skills to better myself for
+        <p>Hello! My name is Seth Martineau and I'm a 26 year old graduate from the University of
+        Utah. I am currently enrolled in a coding bootcamp because I wanted a career change. Coding so far has been
+        challenging and rewarding. I hope to learn more and am able to use these skills to better myself for
       future employment.</p>
 
-            <p>Outside of coding, I considered myself a pretty social person. I love going out on the weekend with my friends, eating out at new restaurants,
-            and shopping for clothes. I also have two Siamese cats named Moses and Ramses and I've been getting into online gaming since COVID started.
+        <p>Outside of coding, I considered myself a pretty social person. I love going out on the weekend with my friends, eating out at new restaurants,
+        and shopping for clothes. I also have two Siamese cats named Moses and Ramses and I've been getting into online gaming since COVID started.
       Currently I've been playing a lot of <a class="text-white" href="https://na.leagueoflegends.com/en-us/">League of Legends</a>. Hopefully at the end of this journey,
       I am able to use my skills to find a job and increase my in game rank, haha, and stay slim......Anyways that's enough about me,
       if you want to know more, you'll have to look through my portfolio!</p>
 
-          </div>
-        </div>
-      </div1>
-
+      </div>
 
     </divabout>
   )
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -13,71 +13,80 @@ function Portfolio() {
   return (
     <divportfolio>
 
-      <div1 class="card-deck auto p-3 text-center paper">
-        <div class="card">
-          <img src={resume} class="card-img-top" alt="Resume"></img>
-          <div class="card-body text-center text-white lavendar">
-            <h5 class="card-title text-center">Resume</h5>
-            <p><a class="card-text text-center text-white" href={resumePDF}>View</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={smartestAmongUs} class="card-img-top" alt="Smartest Among Us"></img>
-          <div class="card-body text-center text-white overcast">
-            <h5 class="card-title text-center">Smartest Among Us</h5>
-            <p><a class="card-text text-center text-white text-white" href="https://the-smartest-amoung-us.herokuapp.com/">Live Site</a></p>
-            <p><a class="card-text text-center text-white text-white" href="https://github.com/slothings/the-smartest-among-us">GitHub Repository</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={qrAidCodes} class="card-img-top" alt="QR-Aid-Codes"></img>
-          <div class="card-body text-center text-white lavendar">
-            <h5 class="card-title text-center">QR-Aid-Codes</h5>
-            <p><a class="card-text text-center text-white" href="https://diegopie.github.io/QR-aid-Codes/">Live Site</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/qr-aid-codes">GitHub Repository</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={readmeGenerator} class="card-img-top" alt="README Generator"></img>
-          <div class="card-body text-center text-white overcast">
-            <h5 class="card-title text-center">README Generator</h5>
-            <p><a class="card-text text-center text-white" href="https://drive.google.com/file/d/1TNf6tot9HryqUInftu65i-4acMOm0ygH/view">Tutorial Video</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/readme-generator">GitHub Repository</a></p>
+      <div1 class="row">
+        <div class="col">
+          <div class="card-deck auto p-3 text-center paper">
+
+            <div class="card">
+              <img src={resume} class="card-img-top img-fluid" alt="Resume"></img>
+              <div class="card-body text-center text-white lavendar">
+                <h5 class="card-title text-center">Resume</h5>
+                <p><a class="card-text text-center text-white" href={resumePDF}>View</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={smartestAmongUs} class="card-img-top img-fluid" alt="Smartest Among Us"></img>
+              <div class="card-body text-center text-white overcast">
+                <h5 class="card-title text-center">Smartest Among Us</h5>
+                <p><a class="card-text text-center text-white text-white" href="https://the-smartest-amoung-us.herokuapp.com/">Live Site</a></p>
+                <p><a class="card-text text-center text-white text-white" href="https://github.com/slothings/the-smartest-among-us">GitHub Repository</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={qrAidCodes} class="card-img-top img-fluid" alt="QR-Aid-Codes"></img>
+              <div class="card-body text-center text-white lavendar">
+                <h5 class="card-title text-center">QR-Aid-Codes</h5>
+                <p><a class="card-text text-center text-white" href="https://diegopie.github.io/QR-aid-Codes/">Live Site</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/qr-aid-codes">GitHub Repository</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={readmeGenerator} class="card-img-top img-fluid" alt="README Generator"></img>
+              <div class="card-body text-center text-white overcast">
+                <h5 class="card-title text-center">README Generator</h5>
+                <p><a class="card-text text-center text-white" href="https://drive.google.com/file/d/1TNf6tot9HryqUInftu65i-4acMOm0ygH/view">Tutorial Video</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/readme-generator">GitHub Repository</a></p>
+              </div>
+            </div>
           </div>
         </div>
       </div1>
 
-      <div2 class="card-deck auto p-3 text-center paper">
-        <div class="card">
-          <img src={noteTaker} class="card-img-top" alt="Note Taker"></img>
-          <div class="card-body text-center text-white overcast">
-            <h5 class="card-title text-center">Note Taker</h5>
-            <p><a class="card-text text-center text-white" href="https://vast-island-67845.herokuapp.com/">Live Site</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/note-taker">GitHub Repository</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={teamProfileGenerator} class="card-img-top" alt="Team Profile Generator"></img>
-          <div class="card-body text-center text-white lavendar">
-            <h5 class="card-title text-center">Team Profile Generator</h5>
-            <p><a class="card-text text-center text-white" href="https://drive.google.com/file/d/1nGHiDMc6LsH1qZbPwOeMPSxv4mOBGq0y/view">Tutorial Video</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/team-profile-generator">GitHub Repository</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={passwordGenerator} class="card-img-top" alt="Password Generator"></img>
-          <div class="card-body text-center text-white overcast">
-            <h5 class="card-title text-center">Password Generator</h5>
-            <p><a class="card-text text-center text-white" href="https://slothings.github.io/password-generator/">Live Site</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/password-generator">GitHub Repository</a></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src={workDayScheduler} class="card-img-top" alt="Work Day Scheduler"></img>
-          <div class="card-body text-center text-white lavendar">
-            <h5 class="card-title text-center">Work Day Scheduler</h5>
-            <p><a class="card-text text-center text-white" href="https://slothings.github.io/work-day-scheduler/">Live Site</a></p>
-            <p><a class="card-text text-center text-white" href="https://github.com/slothings/work-day-scheduler">GitHub Repository</a></p>
+      <div2 class="row">
+        <div class="col">
+          <div class="card-deck auto p-3 text-center paper">
+            <div class="card">
+              <img src={noteTaker} class="card-img-top img-fluid" alt="Note Taker"></img>
+              <div class="card-body text-center text-white overcast">
+                <h5 class="card-title text-center">Note Taker</h5>
+                <p><a class="card-text text-center text-white" href="https://vast-island-67845.herokuapp.com/">Live Site</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/note-taker">GitHub Repository</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={teamProfileGenerator} class="card-img-top img-fluid" alt="Team Profile Generator"></img>
+              <div class="card-body text-center text-white lavendar">
+                <h5 class="card-title text-center">Team Profile Generator</h5>
+                <p><a class="card-text text-center text-white" href="https://drive.google.com/file/d/1nGHiDMc6LsH1qZbPwOeMPSxv4mOBGq0y/view">Tutorial Video</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/team-profile-generator">GitHub Repository</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={passwordGenerator} class="card-img-top img-fluid" alt="Password Generator"></img>
+              <div class="card-body text-center text-white overcast">
+                <h5 class="card-title text-center">Password Generator</h5>
+                <p><a class="card-text text-center text-white" href="https://slothings.github.io/password-generator/">Live Site</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/password-generator">GitHub Repository</a></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src={workDayScheduler} class="card-img-top img-fluid" alt="Work Day Scheduler"></img>
+              <div class="card-body text-center text-white lavendar">
+                <h5 class="card-title text-center">Work Day Scheduler</h5>
+                <p><a class="card-text text-center text-white" href="https://slothings.github.io/work-day-scheduler/">Live Site</a></p>
+                <p><a class="card-text text-center text-white" href="https://github.com/slothings/work-day-scheduler">GitHub Repository</a></p>
+              </div>
+            </div>
           </div>
         </div>
       </div2>
